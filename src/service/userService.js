75203d74ac920854f10e7331c4a87e2f99ced04d
Minh Manh <manhminh2601@gmail.com>
2023-08-19 21:49:35 +0700

    orm create user

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,29 +1,24 @@
 import mysql from 'mysql2/promise';
 import bcrypt from 'bcryptjs';
 import bluebird from 'bluebird';
+import db from '../models';
 
 // init salt
 const salt = bcrypt.genSaltSync(10);
-
 
 const hashUserPassword = (password) => {
     let hashPassword = bcrypt.hashSync(password, salt);
     return hashPassword;
 }
 
-
 const createNewUser = async (email, password, username) => {
     let hashPassword = hashUserPassword(password);
-    // create the connection, specify bluebird as Promise
-    const connection = await mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        database: 'jwt',
-        Promise: bluebird
-    });
-
     try {
-        const [rows, fields] = await connection.execute('Insert into  users(email, password, username) Value(?, ?, ?)', [email, hashPassword, username],);
+        await db.User.create({
+            email: email,
+            password: hashPassword,
+            username: username
+        })
     } catch (error) {
         console.log('check error', error);
     }
@@ -39,7 +34,7 @@ const getUserList = async () => {
     });
 
     try {
-        const [rows, fields] = await connection.execute('Select * from  users');
+        const [rows, fields] = await connection.execute('Select * from  user');
         return rows;
     } catch (error) {
         console.log('check error', error);
@@ -55,7 +50,7 @@ const deleteUser = async (id) => {
         Promise: bluebird
     });
     try {
-        const [rows, fields] = await connection.execute('DELETE FROM users WHERE id=?', [id]);
+        const [rows, fields] = await connection.execute('DELETE FROM user WHERE id=?', [id]);
         return rows;
     } catch (error) {
         console.log('check error', error);
@@ -71,7 +66,7 @@ const getUserById = async (id) => {
         Promise: bluebird
     });
     try {
-        const [rows, fields] = await connection.execute('Select * from users WHERE id=?', [id]);
+        const [rows, fields] = await connection.execute('Select * from user WHERE id=?', [id]);
         return rows;
     } catch (error) {
         console.log('check error', error);
@@ -88,7 +83,7 @@ const updateUser = async (email, username, id) => {
         Promise: bluebird
     });
     try {
-        const [rows, fields] = await connection.execute('UPDATE users SET email = ?, username= ? WHERE id = ?', [email, username, id]);
+        const [rows, fields] = await connection.execute('UPDATE user SET email = ?, username= ? WHERE id = ?', [email, username, id]);
         console.log(rows);
         return rows;
     } catch (error) {
